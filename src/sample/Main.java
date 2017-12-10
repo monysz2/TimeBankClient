dd84dc961f24b0e2c5e1737368dd1140adcd47e7
monysz2 <sz.maslowski@hotmail.com>
2017-12-10 18:07:06 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner toSend = new Scanner(System.in);
+        Socket echoSocket = new Socket("127.0.0.1", 6666);
+        Scanner sc = new Scanner(echoSocket.getInputStream());
+        PrintWriter printer = new PrintWriter(echoSocket.getOutputStream(),true);
+        while(true)
+        {
+            String out = sc.nextLine();
+            System.out.println(out);
+            if(out.equals("> "))
+            {
+                String send = toSend.nextLine();
+                printer.println(send);
+
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+}
